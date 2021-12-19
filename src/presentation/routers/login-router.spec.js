f6f6280aaba89fb1848bbcde9bf853b9382422cf
jonathanmachado <jonathanmachado48@gmail.com>
2021-12-19 18:30:39 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -28,6 +28,15 @@ const makeEmailValidator = () => {
   return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+    isValid () {
+      throw new Error()
+    }
+  }
+  const emailValidatorSpy = new EmailValidatorSpy()
+  return emailValidatorSpy
+}
 const makeAuthUseCaseWithError = () => {
   class AuthUseCaseSpy {
     async auth () {
@@ -199,6 +208,22 @@ describe('Login Route', () => {
   test('Should return 500 if EmailValidatos has no isValid method', async () => {
     const authUseCaseSpy = makeAuthUseCase()
     const sut = new LoginRouter(authUseCaseSpy, {})
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if EmailValidatos throws', async () => {
+    const authUseCaseSpy = makeAuthUseCase()
+    const emailValidatorSpy = makeEmailValidatorWithError()
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
 
     const httpRequest = {
       body: {
