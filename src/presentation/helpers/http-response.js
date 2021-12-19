67e6e6693669ac9e68964188712d45481de548ff
jonathanmachado <jonathanmachado48@gmail.com>
2021-12-19 13:07:40 -0300

    move production code to their own files

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,15 @@
+const MissingParamError = require('./missing-params-error.js')
+module.exports = class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
