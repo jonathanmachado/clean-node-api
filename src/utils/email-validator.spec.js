0f3f4d7313c8425af939d7711d77a174cde78dc5
jonathanmachado <jonathanmachado48@gmail.com>
2021-12-21 21:15:49 -0300

    ensure EmailValidator returns false if validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,7 +1,7 @@
+const validator = require('validator')
 class EmailValidator {
   isValid (email) {
-    this.email = email
-    return true
+    return validator.isEmail(email)
   }
 }
 describe('Email Validator', () => {
@@ -9,5 +9,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+    expect(isEmailValid).toBe(false)
   })
 })
